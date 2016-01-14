552d37ab41164dd295c6a9f8aceea2aa8bde52fc
Takumi Shimojo <shimojo.t.ab@m.titech.ac.jp>
2016-01-14 12:43:11 +0900

    fix bug

--- app/models/UserProject.java
+++ app/models/UserProject.java
@@ -64,6 +64,7 @@ public class UserProject extends Model {
 		UserProject up = UserProject.find("user_id = ? AND project_id = ?", user_id, project_id).first();
 		if(up == null) return false;
 		return up.registered;
+	}
 
 	public static boolean checkUserProject(Long user_id, Long project_id){
 		List<UserProject> list = UserProject.find("project_id = ?", project_id).fetch();
